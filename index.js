3f6a8469938d796326917bc68ab7a117e2817d5f
Miguel Nazario <mnazario35@gmail.com>
2022-02-09 15:22:19 -0500

    commit tree 4c3ac14f1e013d9d631ea565213f39815611a30e

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+const userObj = {
+    name : "",
+    email : ""
+}
+const postUserObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify(userObj), 
+};
+
+function submitData(stringName, stringEmail) {
+    userObj.name = stringName;
+    userObj.email = stringEmail;
+    postUserObj.body = JSON.stringify(userObj);
+    return fetch("http://localhost:3000/users", postUserObj) 
+    .then(response => response.json())
+    .then(object => {
+        console.log(object);
+        document.body.innerHTML = object.id;
+        console.log(document.body.innerHTML)
+    })
+    .catch(error => {
+        console.log(error.message)
+        document.body.append(error.message);
+    })
+}
